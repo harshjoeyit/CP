7b2a2f9a621dc6b49323a241d389f222ca063526
harshjoeyit <harshit02gangwar@gmail.com>
2019-12-14 23:22:16 +0530

    Graph , strings algo

---
+++ Graph_multisource_BFS.cpp
@@ -0,0 +1,86 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll                    long long int
+#define ld                    long double
+#define mid(s,e)              (s+(e-s)/2)
+#define MOD                   1000000007
+#define F                     first
+#define S                     second
+#define mp                    make_pair
+#define pb                    push_back
+#define eb                    emplace_back
+#define itoc(c)               ((char)(((int)'0')+c))
+#define vi                    vector<int>
+#define pll                   pair<ll,ll>
+#define pii                   pair<int,int>
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int n;
+    cin >> n;
+
+    vi G[n+1];
+    vi a(n+1);
+    vi dis(n+1);
+    vector<bool> vis(n+1);
+
+    for(int i = 1; i <= n; i++)
+    {
+        cin >> a[i]; 
+        vis[i] = false;
+        dis[i] = -1;
+
+        if(i - a[i] >= 1)
+            G[i - a[i]].pb(i);
+
+        if(i + a[i] <= n)
+            G[i + a[i]].pb(i);
+    }
+
+    set<int> s;
+    for(int u = 1; u <= n; u++)
+    {
+        for(int v : G[u])
+        {
+            if((a[u] + a[v]) % 2)           // finding those vertces that can be visited in single step
+            {
+                s.insert(v);
+            }
+        }
+    }
+
+    queue<int> q;                   // all other vertices will be visited from the 4
+                                    //vertices that are in the set 
+    for(int i: s)                   // and distances add on at each step 
+    {
+        q.push(i);
+        dis[i] = 1;
+        vis[i] = true;
+    }
+
+    while(! q.empty())
+    {
+        int u = q.front();
+        q.pop();
+
+        for(int v: G[u])
+        {
+            if(! vis[v])
+            {
+                vis[v] = true;
+                dis[v] = dis[u] + 1;
+                q.push(v);
+            }
+        }
+    }
+
+    for(int i = 1; i <= n; i++)
+        cout << dis[i] << " ";
+    cout << endl;
+}
--- substring_searching_algorithm.cpp
+++ substring_searching_algorithm.cpp
@@ -1,27 +1,25 @@
 #include <bits/stdc++.h> 
 using namespace std; 
   
-// Returns true if s1 is substring of s2 
-int isSubstring(string s1, string s2) 
-{ 
-    int M = s1.length(); 
-    int N = s2.length(); 
 
-    for (int i = 0; i <= N - M; i++) { 
-        int j; 
-  
-        /* For current index i, check for pattern match */
-        for (j = 0; j < M; j++) 
-            if (s2[i + j] != s1[j]) 
-                break; 
-  
-        if (j == M) 
-            return i; 
-    } 
-  
-    return -1; 
-} 
-  
+int substring(string str, string substr)
+{
+    int N = str.length();
+    int M = substr.length();
+
+    for(int i = 0; i <= N - M; i++)
+    {
+        int j;
+
+        for(j = 0; j < M; j++)
+            if(str[i+j] != substr[j])
+                break;
+        
+        if(j == M)
+            return i;
+    }
+    return -1;
+}
   
 int main() 
 { 
@@ -31,7 +29,7 @@ int main()
     cout<<"enter the sub-string ";
     getline(cin , s1);
 
-    int res = isSubstring(s1, s2); 
+    int res = substring(s1, s2); 
     if (res == -1) 
         cout << "Not present"; 
     else
