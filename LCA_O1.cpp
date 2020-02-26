5711ab901fb297af7564ea9b6ce54ae752e34a55
harshjoeyit <harshit02gangwar@gmail.com>
2020-02-26 17:33:09 +0530

    update algos

--- LCA_O1.cpp
+++ LCA_O1.cpp
@@ -1,132 +1,108 @@
-
 //..................LCA - O(1)....................
 
 #include<bits/stdc++.h> 
 using namespace std; 
-#define sz 101 
-  
-vector < int > adj[sz];    
-vector < int > euler;      
-vector < int > depthArr;    
-  
-int FAI[sz];     // stores first appearence index of every node 
-int level[sz];   
-int ptr;         // pointer to euler walk 
-int dp[sz][18];  // sparse table 
-int logn[sz];    // stores log values 
-int p2[20];      // stores power of 2 
 
+const int sz = 5e5+5;
 
-
-void buildSparseTable(int n) 
-{ 
-    memset(dp,-1,sizeof(dp)); 
-  
-    for (int i = 1; i < n; i++) 
-        dp[i-1][0] = (depthArr[i]>depthArr[i-1])?i-1:i; 
-  
-    // dp to fill sparse table 
-    for (int l=1; l<15; l++) 
-      for (int i=0; i<n; i++) 
-        if (dp[i][l-1]!=-1 and dp[i+p2[l-1]][l-1]!=-1) 
-        {
-            dp[i][l] = (depthArr[dp[i][l-1]] > depthArr[dp[i+p2[l-1]][l-1]]) ? dp[i+p2[l-1]][l-1] : dp[i][l-1]; 
-            //cout << i << "-" << p2[l] << " -> " << i << "-" << p2[l-1] << " + " << i+p2[l-1]<< "-" << endl;
-        }
-        else
-             break; 
-} 
-
-
-int query(int l,int r) 
-{ 
-    int d = r-l; 
-    int dx = logn[d]; 
-    if (l == r) 
-        return l; 
-    
-    //cout << l << "-" << r << " -> " << l << "-" << p2[dx] << " + " << r-p2[dx]<< "-" << r << endl;
-
-    if (depthArr[dp[l][dx]] > depthArr[dp[r-p2[dx]][dx]]) 
-        return dp[r-p2[dx]][dx]; 
-    else
-        return dp[l][dx]; 
-} 
-
-
-
-// memoization so that time complexity can be reduced
-void preprocess() 
-{ 
-    // memorizing powers of 2 
-    p2[0] = 1; 
-    for (int i=1; i<18; i++) 
-        p2[i] = p2[i-1]*2; 
-  
-    // memorizing all log(n) values 
-    int val = 1, ptr = 0; 
-    for (int i=1; i<sz; i++) 
-    { 
-        logn[i] = ptr-1; 
-        if (val==i) 
-        { 
-            val*=2; 
-            logn[i] = ptr; 
-            ptr++; 
-        } 
-    } 
-} 
+vector<int> G[sz];    
+vector<int> euler;            
+int fiocc[sz];     	
+int depth[sz]; 
+int lookup[sz][18];  
+int dis[sz];
 
 
 void dfs(int u, int p, int dep) 
 { 
-    if (FAI[u] == -1) 
-        FAI[u] = ptr; 
-  
-    level[u] = dep; 
-    euler.push_back(u); 
-    ptr++; 
-  
-    for (auto v : adj[u]) 
-    { 
-        if (v != p) 
-        { 
-            dfs(v , u, dep+1); 
-            euler.push_back(u);         // push while backtracking
-            ptr++; 
-        } 
-    } 
-} 
-  
-// Create Level depthArray corresponding 
-// to the Euler walk Array 
-void makeArr() 
+    fiocc[u] = euler.size();
+    depth[u] = dep;
+    for(int v: G[u])
+		if(v != p)
+		{
+        	euler.push_back(u);
+        	dfs(v, u, dep+1);
+    	}
+    euler.push_back(u);
+}
+
+void buildSparseTable() 
 { 
-    for (auto x : euler) 
-        depthArr.push_back(level[x]); 
-} 
-  
-int LCA(int u,int v) 
-{ 
-    // trival case 
-    if (u==v) 
-       return u; 
-  
-    if (FAI[u] > FAI[v]) 
-       swap(u,v); 
-  
-    // doing RMQ in the required range 
-    int indx = query(FAI[u], FAI[v]);
-    return euler[indx]; 
-} 
+    memset(lookup, -1, sizeof(lookup)); 
+	dfs(1,-1,0);
+	
+    int n = euler.size();
+    for(int i=0; i<n; i++)
+        lookup[i][0] = euler[i];
+
+    for(int i=1; i<20; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+			int x = lookup[j][i-1];
+			int y = lookup[j+(1<<(i-1))][i-1];
+            if(x == -1 || y == -1)
+                continue;
+            if(depth[x] < depth[y]) 
+                lookup[j][i] = x;
+            else 
+                lookup[j][i] = y;
+        }
+    }
+}
 
 
-void addEdge(int u,int v) 
-{ 
-    adj[u].push_back(v); 
-    adj[v].push_back(u); 
-} 
-  
+int LCA(int a, int b)
+{
+    int l = fiocc[a];
+    int r = fiocc[b];
+    if(l > r)
+		swap(l, r);
+    int d = log2(r-l+1);
+
+	int x = lookup[l][d];
+	int y = lookup[r-(1<<d)+1][d];
+	
+	if(depth[x] < depth[y])
+		return x;
+    return y;
+}
+
+
+void LCA_query(int q)
+{
+    cout << "enter x, y for LCA(x, y): \n"; 
+    while(q--)
+    {
+        int x, y;
+        cin >> x >> y;
+        cout << LCA(x, y) << endl;
+    }
+}
+
+
+void dfs1(int u, int p)
+{
+    for(auto v: G[u])
+        if(v != p)
+        {
+            dis[v] = dis[u] + 1;
+            dfs1(v, u);
+        }
+}
+
+
+void dist_between(int q)
+{
+    cout << "enter x, y for dis(x, y): \n"; 
+    while(q--)
+    {
+        int a, b;
+        cin >> a >> b;
+        int lca = LCA(a, b);
+        cout << (dis[a] + dis[b] - 2 * dis[lca]) << endl;
+    }
+}
 
 
 int main(int argc, char const *argv[]) 
@@ -138,30 +114,16 @@ int main(int argc, char const *argv[])
     {
         int x, y;
         cin >> x >> y;
-        addEdge(x, y);
+        G[x].push_back(y); 
+    	G[y].push_back(x); 
     }
-  
-    // performing required precalculations 
-    preprocess(); 
-  
-    // doing the Euler walk 
-    ptr = 0; 
-    memset(FAI,-1,sizeof(FAI)); 
-    dfs(1,0,0); 
-  
-    // creating depthArray corresponding to euler[] 
-    makeArr(); 
-  
-    // building sparse table 
-    buildSparseTable(depthArr.size()); 
-    
-    while(q--)
-    {
-        int x, y;
-        cin >> x >> y;
-        cout << LCA(x, y) << endl;
-    }
+	
+    buildSparseTable();
+	// lca in O(1)
+ 	LCA_query(q);
+    // finding distance between any two nodes in O(1)
+   	dfs1(1, 0);
+   	dist_between(q);
   
     return 0; 
 } 
- 
--- eulerTree.cpp
+++ eulerTree.cpp
@@ -6,7 +6,8 @@ using namespace std;
 const int N = 10000;
 vector<int> G[N];
 int vis[N];
-int Euler[2*N];
+int euler[2*N];
+vector<pair<int, int>> first_last(N);
 
 
 void eulerTree(int u, int &indx) 
@@ -17,19 +18,39 @@ void eulerTree(int u, int &indx)
         if (!vis[it]) { 
             eulerTree(it, indx);        // calling recursive on one of the subtress of u
             euler[indx++] = u;          // after call the path comes back to the parent node so print again 
-        }                               // any parent node is printed 1+number of children times 
+        }                               // any parent node is printed (1+number of children times) 
     } 
 }	
 
 
-void printEulerTour(int root, int n) 
+void printeulerTour(int root, int n) 
 { 
     int index = 0;   
     eulerTree(root, index); 
     for(int i = 0; i < (2*n-1); i++) 
-        cout << Euler[i] << " "; 
+        cout << euler[i] << " "; 
 } 
   
+
+void calc_subtree_nodes(int n)
+{
+
+    for(int i = 1; i <= n; i++)
+        first_last[i].first = first_last[i].second = -1;
+
+    for(int i = 0; i < 2*n-1; i++)
+    {
+        int v = euler[i];
+        if(first_last[v].first == -1)
+            first_last[v].first = first_last[v].second = i;
+        else
+            first_last[v].second = i;
+    }
+
+    cout << endl;
+    for(int i = 1; i <= n; i++)
+        cout << i << "- "<< (first_last[i].second - first_last[i].first)/2 << endl;
+}
 
 
 
@@ -46,5 +67,6 @@ signed main()
 		G[y].push_back(x);
 	}
 
-    printEulerTour(1, n); 
+    printeulerTour(1, n); 
+    calc_subtree_nodes(n);
 }
