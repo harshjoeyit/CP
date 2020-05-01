7f9da6f0721d896d1ef8a3eb3dfa13a269f8595c
harshjoeyit <harshit02gangwar@gmail.com>
2020-05-02 01:46:05 +0530

    PIR + dp

--- josephous_circle_problem.cpp
+++ josephous_circle_problem.cpp
@@ -104,8 +104,52 @@ void josephus_circle2()
 }
 
 
+// using a deque
+int josephus(int n, int k)
+{
+    if(n==1)
+        return 1;
+        
+    deque<int>dq;
+    for(int i=1;i<=n;i++)
+    {
+        dq.push_back(i);
+    }
+    while(dq.size()>1)
+    {
+        for(int i=1;i<k;i++)
+        {
+            int f = dq.front();
+            dq.pop_front();
+            dq.push_back(f);
+        }
+        dq.pop_front();
+    }
+    return dq.front();
+}
 
 
+// using vector 
+int josephus(int n, int k)
+{
+    vector <int> v;
+    for(int i = 0;i<n;i++)
+        v.push_back(i);
+    
+    if(v.size()==1)
+        return 1;
+    int kill = k-1;
+    
+    while(v.size()>1)
+    {
+        v.erase(v.begin()+kill);
+        kill= (kill+k-1)%v.size();
+    }
+    return v[0] +1;
+}
+
+
+// O(n) solution 
 
 
 
